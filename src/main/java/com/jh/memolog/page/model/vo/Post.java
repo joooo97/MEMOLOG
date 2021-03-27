9a5f80affedcbd743936b96c38707eb6fac2f907
joooo97 <68749697+joooo97@users.noreply.github.com>
2021-03-28 02:20:05 +0900

    포스트 관련 기능 커밋
    - 페이지별 포스트(텍스트/첨부파일/테이블) 조회, 생성, 수정, 삭제
    - 포스트 고정/고정 해제
    - 첨부파일 포스트 다운로드

---
+++ src/main/java/com/jh/memolog/page/model/vo/Post.java
@@ -0,0 +1,39 @@
+package com.jh.memolog.page.model.vo;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Post implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private int postNo;
+	private int pageNo;
+	private String postSortCode;
+	private String postContent;
+	private String postOriginalFilename;
+	private String postRenamedFilename;
+	private String postWriter;
+	private String postDate;
+	private String postPinnedYn;
+	private String postPinnedPerson;
+	
+	// 가상 컬럼
+	private int commentCount;
+	private String profileRenamedFilename;
+}
+
