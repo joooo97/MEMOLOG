235564241b92ed7bc44d825afdaeb594b77627c8
joooo97 <68749697+joooo97@users.noreply.github.com>
2021-03-28 02:22:00 +0900

    포스트 코멘트 관련 기능 커밋
    - 포스트 코멘트 조회, 생성, 수정, 삭제
    - 포스트 코멘트 답글 작성(댓글 레벨3까지)

---
+++ src/main/java/com/jh/memolog/page/model/vo/PostComment.java
@@ -0,0 +1,35 @@
+package com.jh.memolog.page.model.vo;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class PostComment implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private int postCommentNo;
+	private int pageNo;
+	private int postNo;
+	private String postCommentWriter;
+	private String postCommentLevel;
+	private String postCommentContent;
+	private String postCommentDate;
+	private int postCommentRef;
+	private String postCommentRefWriter;
+	
+	// 가상 컬럼
+	private String profileRenamedFilename;
+}
