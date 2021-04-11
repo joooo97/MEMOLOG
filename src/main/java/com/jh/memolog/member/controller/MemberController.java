143c9be851df49ad99093f4c413f23b2a295122e
joooo97 <68749697+joooo97@users.noreply.github.com>
2021-04-11 15:08:19 +0900

    계정 설정 페이지 이동
    사이드 바 내 계정 설정 메뉴 클릭 시 계정 설정 페이지로 이동

--- src/main/java/com/jh/memolog/member/controller/MemberController.java
+++ src/main/java/com/jh/memolog/member/controller/MemberController.java
@@ -4,14 +4,23 @@ import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.jh.memolog.member.model.exception.MemberException;
+import com.jh.memolog.member.model.service.MemberService;
+import com.jh.memolog.member.model.vo.Member;
 
 @Controller
 public class MemberController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+	
+	@Autowired
+	MemberService memberService;
 	
 	@GetMapping("/signUp")
 	public String signUp() {
@@ -26,11 +35,26 @@ public class MemberController {
 		return "redirect:/";
 	}
 	
-	//테스트용 jsp 이동
-	@GetMapping("/empty")
-	public void empty() {
+	// 계정 설정 페이지로 이동
+	@GetMapping("/account")
+	public ModelAndView account(ModelAndView mav, HttpSession session) {
+		Member memberLoggedIn = (Member)session.getAttribute("memberLoggedIn");
+		String memberId = memberLoggedIn.getMemberId();
+		Member member;
 		
+		try {
+			member = memberService.selectOneMember(memberId);
+		} catch(Exception e) {
+			logger.error("사용자 정보 불러오기 오류!", e);
+			throw new MemberException("사용자 정보 불러오기 오류!", e);
+		}
+		
+		mav.addObject("member", member);
+		mav.setViewName("member/accountSettings");
+		
+		return mav;
 	}
+	
 	
 
 }
